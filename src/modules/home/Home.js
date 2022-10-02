fb3202f417fc1a97d512749303bcf9edac4ea23a
Pratish Joshi <joshipratish997@gmail.com>
2022-10-02 12:24:16 +0545

    Improvement: Added a navigation bar and some transistions for it

--- src/modules/home/Home.js
+++ src/modules/home/Home.js
@@ -1,21 +1,43 @@
-import React from 'react'
-import './home.scss'
+import React from "react";
+import "./home.scss";
 
 const Home = () => {
   return (
-    <section id='home-section'>
-      <div className='grid-container grid-layout'>
-          <div className='left-container'>
-          <h1>Hi, I'm Pratish Joshi</h1>
-          <h1>Full Stack Developer</h1>
+    <section id="home-section">
+      <div className="grid-container">
+        <nav>
+          <span>Pratish</span>
+          <ul>
+            <li>
+              <a href="#home-section">Home</a>
+            </li>
+            <li>
+              <a href="#about-section">About</a>
+            </li>
+            <li>
+              <a href="#portfolio-section">Portfolio</a>
+            </li>
+            <li>
+              <a href="#contact-section">Contact</a>
+            </li>
+            <li>
+              <a href="#footer-section">Footer</a>
+            </li>
+          </ul>
+        </nav>
+        <div className="grid-layout">
+          <div className="left-container">
+            <h1>Hi, I'm Pratish Joshi</h1>
+            <h1>Full Stack Developer</h1>
           </div>
-          <div className='right-container'>
-            <img src='./assets/portfolio.jpg' alt='portfolio image'/>
-            <div className='shadow'></div>
+          <div className="right-container">
+            <img src="./assets/portfolio.jpg" alt="portfolio image" />
+            <div className="shadow"></div>
           </div>
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
